cbf580a012b67bd69a36b7bb5b98f8a9f55ef0e1
Federico <fede.toldo96@gmail.com>
2026-02-17 18:05:32 +0100

    starting page home

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,1 +1,58 @@
 // Swiper Script
+
+window.addEventListener('load', function () {
+    // Progetti
+    new Swiper('.swiperProgetti', {
+        // Optional parameters
+        slidesPerView: 1.5,
+        spaceBetween: 32,
+        loop: true,
+
+        breakpoints: {
+            768: {
+                slidesPerView: 2.5,
+            },
+            1024: {
+                slidesPerView: 3.5,
+            },
+        },
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    // Collaborazioni
+    new Swiper('.swiperCollaborazioni', {
+        // Optional parameters
+        slidesPerView: 2,
+        spaceBetween: 32,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+
+        breakpoints: {
+            768: {
+                slidesPerView: 4,
+            },
+            1024: {
+                slidesPerView: 4,
+            },
+            1280: {
+                slidesPerView: 5,
+            },
+        },
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
--- front-page.php
+++ front-page.php
@@ -50,6 +50,285 @@ get_header(); ?>
 
         </section>
 
+        <section class="servizi-block container">
+            <?php
+            $titolo_servizi = get_field('titolo_servizi_home');
+
+            if ($titolo_servizi) : ?>
+                <h2 class="servizi-block__title">
+                    <?php echo esc_html($titolo_servizi); ?>
+                </h2>
+            <?php endif; ?>
+
+            <div class="servizi-block__loop">
+                <?php /* Servizi Loop */
+
+                $servizi_loop = new WP_Query(array(
+                    'post_type'     => 'servizi',
+                    'posts_per_page' => -1,
+                    'orderby'        => 'menu_order',
+                    'order'          => 'ASC',
+                )); ?>
+
+                <?php if ($servizi_loop->have_posts()) : while ($servizi_loop->have_posts()) : $servizi_loop->the_post(); ?>
+
+                        <?php
+                        $radio_value = get_field('selettore_colore_servizi');
+                        // Normalizza il valore: minuscolo multibyte, spazi -> trattini, poi sanitizza
+                        $radio_slug = '';
+                        if ($radio_value) {
+                            $radio_slug = mb_strtolower($radio_value, 'UTF-8');
+                            $radio_slug = preg_replace('/\s+/', '-', $radio_slug);
+                            $radio_slug = sanitize_html_class($radio_slug);
+                        }
+                        ?>
+
+                        <article class="servizi-block__loop__item <?php echo $radio_slug ? ' ' . esc_attr($radio_slug) : ''; ?>">
+                            <a href="<?php the_permalink(); ?>" class="servizi-block__loop__item__link">
+                                <div class="servizi-block__loop__item__link__image">
+                                    <?php if (has_post_thumbnail()) : ?>
+                                        <?php the_post_thumbnail('large'); ?>
+                                    <?php endif; ?>
+                                </div>
+                                <p class="servizi-block__loop__item__link__title"><?php the_title(); ?></p>
+                            </a>
+                        </article>
+
+                <?php endwhile;
+                endif; ?>
+            </div>
+
+        </section>
+
+        <section class="progetti-block">
+            <div class="progetti-block__wrap container">
+                <div class="progetti-block__wrap__intro">
+                    <?php
+                    // Recupera l'ID della pagina usata come front page (se impostata), altrimenti usa il queried object
+                    $front_id = get_option('page_on_front') ? (int) get_option('page_on_front') : get_queried_object_id();
+
+                    // Recupera i field ACF passando l'ID della pagina frontale per essere sicuri di leggere i valori corretti
+                    $titolo_progetti = get_field('titolo_progetti_home', $front_id);
+                    $link_progetti = get_field('link_progetti_home', $front_id);
+
+                    if ($link_progetti) {
+                        $link_progetti_url = $link_progetti['url'];
+                        $link_progetti_title = $link_progetti['title'];
+                        $link_progetti_target = $link_progetti['target'] ? $link_progetti['target'] : '_self';
+                    }
+                    ?>
+
+                    <span class="spacer">
+                        <?php if ($link_progetti) : ?>
+                            <a href="<?php echo esc_url($link_progetti_url); ?>" target="<?php echo esc_attr($link_progetti_target); ?>" class="progetti-block__wrap__intro__cta">
+                                <?php echo esc_html($link_progetti_title); ?>
+                            </a>
+                        <?php endif; ?>
+                    </span>
+
+                    <?php if ($titolo_progetti) : ?>
+                        <h3 class="progetti-block__wrap__intro__title">
+                            <?php echo esc_html($titolo_progetti); ?>
+                        </h3>
+                    <?php endif; ?>
+
+                    <?php if ($link_progetti) : ?>
+                        <a href="<?php echo esc_url($link_progetti_url); ?>" target="<?php echo esc_attr($link_progetti_target); ?>" class="progetti-block__wrap__intro__cta">
+                            <?php echo esc_html($link_progetti_title); ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+                <div class="progetti-block__wrap__slider swiperProgetti">
+                    <ul class="swiper-wrapper">
+
+                        <?php
+                        // Mostra l'ultimo post del CPT 'giornalino' come primo slide, se esiste.
+                        $giornalino_args = array(
+                            'post_type' => 'giornalino',
+                            'posts_per_page' => 1,
+                            'orderby' => 'date',
+                            'order' => 'DESC',
+                        );
+                        $giornalino_q = new WP_Query($giornalino_args);
+                        if ($giornalino_q->have_posts()) : while ($giornalino_q->have_posts()) : $giornalino_q->the_post(); ?>
+                                <li class="swiper-slide progetti-item">
+                                    <a href="<?php the_permalink(); ?>" class="progetti-item__link">
+                                        <div class="progetti-item__link__image">
+                                            <?php if (has_post_thumbnail()) : ?>
+                                                <?php the_post_thumbnail('large'); ?>
+                                            <?php endif; ?>
+                                        </div>
+                                        <time class="progetti-item__link__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>"><?php echo esc_html(get_the_date()); ?></time>
+                                        <p class="progetti-item__link__title"><?php the_title(); ?></p>
+                                    </a>
+                                </li>
+                        <?php endwhile;
+                            wp_reset_postdata();
+                        endif; ?>
+
+                        <?php /* Progetti Loop */
+
+                        $progetti_loop = new WP_Query(array(
+                            'post_type'     => 'progetti',
+                            'posts_per_page' => -1,
+                            'orderby'        => 'menu_order',
+                            'order'          => 'ASC',
+                        )); ?>
+
+                        <?php if ($progetti_loop->have_posts()) : while ($progetti_loop->have_posts()) : $progetti_loop->the_post(); ?>
+
+                                <li class="swiper-slide progetti-item">
+                                    <a href="<?php the_permalink(); ?>" class="progetti-item__link">
+                                        <div class="progetti-item__link__image">
+                                            <?php if (has_post_thumbnail()) : ?>
+                                                <?php the_post_thumbnail('medium'); ?>
+                                            <?php endif; ?>
+                                        </div>
+                                        <time class="progetti-item__link__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>"><?php echo esc_html(get_the_date()); ?></time>
+                                        <p class="progetti-item__link__title"><?php the_title(); ?></p>
+                                    </a>
+                                </li>
+
+                                <?php wp_reset_postdata(); ?>
+                        <?php endwhile;
+                        endif; ?>
+                    </ul>
+
+                    <div class="swiper-button-prev"></div>
+
+                    <div class="swiper-button-next"></div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="collaborazioni-block container ">
+            <div class="collaborazioni-block__wrap">
+                <div class="collaborazioni-block__wrap__intro">
+                    <?php
+                    $title_collab = get_field('titolo_collaborazioni_home');
+                    if ($title_collab) : ?>
+                        <h4 class="collaborazioni-block__wrap__intro__title"><?php echo esc_html($title_collab); ?></h4>
+                    <?php endif; ?>
+                </div>
+
+                <?php
+                if (have_rows('repeater_collaboratori_home')): ?>
+
+                    <div class="collaborazioni-block__wrap__repeater swiperCollaborazioni">
+                        <ul class="swiper-wrapper">
+
+                            <?php
+                            while (have_rows('repeater_collaboratori_home')) : the_row();
+                                $link_rep_collab = get_sub_field('link');
+                                $image_rep_collab = get_sub_field('image');
+
+                                if ($link_rep_collab) {
+                                    $link_rep_collab_url = $link_rep_collab['url'];
+                                    $link_rep_collab_title = $link_rep_collab['title'];
+                                    $link_rep_collab_target = $link_rep_collab['target'] ? $link_rep_collab['target'] : '_self';
+                                }; ?>
+
+                                <li class="collab-item swiper-slide">
+
+                                    <?php if ($link_rep_collab) : ?>
+                                        <a href="<?php echo esc_url($link_rep_collab_url); ?>" target="<?php echo esc_attr($link_rep_collab_target); ?>" class="collab-item__link">
+                                            <?php if ($image_rep_collab) : ?>
+                                                <img src="<?php echo esc_url($image_rep_collab['url']); ?>" alt="<?php echo esc_attr($image_rep_collab['alt']); ?>">
+                                            <?php endif; ?>
+                                        </a>
+                                    <?php elseif ($image_rep_collab) : ?>
+                                        <img src="<?php echo esc_url($image_rep_collab['url']); ?>" alt="<?php echo esc_attr($image_rep_collab['alt']); ?>">
+                                    <?php endif; ?>
+
+                                </li>
+
+
+                            <?php
+                            endwhile; ?>
+
+                        </ul>
+
+                        <div class="swiper-button-prev"></div>
+
+                        <div class="swiper-button-next"></div>
+
+                        <div class="swiper-pagination"></div>
+
+                    </div>
+
+                <?php
+                endif; ?>
+            </div>
+        </section>
+
+        <section class="news-block">
+            <div class="news-block__wrap container">
+                <div class="news-block__wrap__intro">
+                    <?php
+                    // Recupera l'ID della pagina usata come front page (se impostata), altrimenti usa il queried object
+                    $front_id = get_option('page_on_front') ? (int) get_option('page_on_front') : get_queried_object_id();
+
+                    // Recupera i field ACF passando l'ID della pagina frontale per essere sicuri di leggere i valori corretti
+                    $titolo_news = get_field('titolo_news_home', $front_id);
+                    $link_news = get_field('link_news_home', $front_id);
+
+                    if ($link_news) {
+                        $link_news_url = $link_news['url'];
+                        $link_news_title = $link_news['title'];
+                        $link_news_target = $link_news['target'] ? $link_news['target'] : '_self';
+                    }
+                    ?>
+
+                    <span class="spacer">
+                        <?php if ($link_news) : ?>
+                            <a href="<?php echo esc_url($link_news_url); ?>" target="<?php echo esc_attr($link_news_target); ?>" class="news-block__wrap__intro__cta">
+                                <?php echo esc_html($link_news_title); ?>
+                            </a>
+                        <?php endif; ?>
+                    </span>
+
+                    <?php if ($titolo_news) : ?>
+                        <h3 class="news-block__wrap__intro__title">
+                            <?php echo esc_html($titolo_news); ?>
+                        </h3>
+                    <?php endif; ?>
+
+                    <?php if ($link_news) : ?>
+                        <a href="<?php echo esc_url($link_news_url); ?>" target="<?php echo esc_attr($link_news_target); ?>" class="news-block__wrap__intro__cta">
+                            <?php echo esc_html($link_news_title); ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+                <ul class="news-block__wrap__loop">
+                    <?php /* Post Loop */
+
+                    $post_loop = new WP_Query(array(
+                        'post_type'     => 'post',
+                        'posts_per_page' => 4,
+                        'orderby'        => 'menu_order',
+                        'order'          => 'ASC',
+                    )); ?>
+
+                    <?php if ($post_loop->have_posts()) : while ($post_loop->have_posts()) : $post_loop->the_post(); ?>
+                            <li class="news-block__wrap__loop__item">
+                                <a href="<?php the_permalink(); ?>" class="news-block__wrap__loop__item__link">
+                                    <div class="news-block__wrap__loop__item__link__image">
+                                        <?php the_post_thumbnail('large', array('class' => '', 'alt' => get_the_title())); ?>
+                                    </div>
+                                    <time class="news-block__wrap__loop__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>"><?php echo esc_html(get_the_date()); ?></time>
+                                    <p><?php the_title(); ?></p>
+                                </a>
+                            </li>
+
+                    <?php endwhile;
+                        wp_reset_postdata();
+                    endif; ?>
+
+                </ul>
+            </div>
+        </section>
+
         <section class="placeholder">
         </section>
 
